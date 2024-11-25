24ef96a84799abcf5b10b4bec1744ebc3f4d4dbb
AAYUSH ANAND SINHA <98117714+Aayushsinha09@users.noreply.github.com>
2024-11-25 15:09:55 +0530

    Add files via upload

---
+++ customer_table.py
@@ -0,0 +1,52 @@
+import sqlite3
+import pandas as pd 
+
+
+def display_table(table_name, conn):
+    """
+    Fetches data from a specified table and displays it as a DataFrame.
+    
+    Args:
+        table_name (str): The name of the table to fetch.
+        conn (sqlite3.Connection): SQLite connection object.
+    """
+    try:
+     
+        query = f"SELECT * FROM {table_name};"
+        table_data = pd.read_sql_query(query, conn)
+        
+     
+        if not table_data.empty:
+            print(f"\nData from table '{table_name}':")
+            print(table_data.to_string(index=False))  # Print DataFrame without row indices
+        else:
+            print(f"\nTable '{table_name}' is empty.")
+    except Exception as e:
+        print(f"Error fetching table '{table_name}': {e}")
+
+def display_all_tables(conn):
+    """
+    Displays all the tables in the database.
+    
+    Args:
+        conn (sqlite3.Connection): SQLite connection object.
+    """
+  
+    table = ["customers",] 
+              
+    for table in table:
+        display_table(table, conn)
+
+
+if __name__ == "__main__":
+    try:
+   
+        conn = sqlite3.connect('walmart_sales.db')
+        
+
+        display_all_tables(conn) # type: ignore
+    except Exception as e:
+        print(f"Error connecting to the database: {e}")
+    finally:
+    
+        conn.close()
